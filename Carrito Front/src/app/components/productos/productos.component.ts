eaf3d98a1501f35197aeb9d7f95d83b5d9b14258
AgusGelos <juanagustingelos1@gmail.com>
2021-04-21 09:51:04 -0300

    mock creado, inicio de Input

--- Carrito Front/src/app/components/productos/productos.component.ts
+++ Carrito Front/src/app/components/productos/productos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 
 @Component({
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./productos.component.css']
 })
 export class ProductosComponent implements OnInit {
+
+  @Input() columnsName: string [];
 
 
   EstadoForm: string;
